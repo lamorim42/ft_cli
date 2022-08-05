18b7fcbf0b8779f566870786d2242a3418176d26
luishads <luishads@gmail.com>
2022-08-05 18:00:11 -0300

    feat: directory project structed
    project_name dir
    	app
    		include
    			project_name.h
    		src
    			main.c
    	Makefile
    	README.md
    	.gitignore

--- app/include/ft_cli.h
+++ app/include/ft_cli.h
@@ -11,6 +11,7 @@
 # include <sys/stat.h>
 # include <sys/types.h>
 # include <dirent.h>
+# include <fcntl.h>
 
 typedef struct s_cli
 {
--- app/src/building_project.c
+++ app/src/building_project.c
@@ -1,34 +1,37 @@
 
 #include "ft_cli.h"
 
-static int	make_dir(cli *data, char *cwd);
+static int	make_dir(char *cwd);
 static char	*make_path_dir(char *cwd, char *name);
+static void	make_structs(cli *data);
+static void	make_file(char *path, char *file);
+static void	make_dir_structs(cli *data);
+static void	make_main(cli *data);
+static void	make_include(cli *data);
 
 void	building_project(cli *data)
 {
 	char	cwd[500];
 
 	getcwd(cwd, 500);
-	if (!make_dir(data, cwd));
+	data->dir = make_path_dir(cwd, data->name);
+	if (make_dir(data->name) == 0)
 		make_structs(data);
 }
 
-static int	make_dir(cli *data, char *cwd)
+static int	make_dir(char *pathdir)
 {
 	DIR	*dir;
 
-	dir = opendir(data->name);
+	dir = opendir(pathdir);
 	if (dir)
 	{
-		dprintf(2, "%s: Directory exists\n", data->name);
+		dprintf(2, "%s: Directory exists\n", pathdir);
 		closedir(dir);
 		return (-1);
 	}
 	else
-	{
-		data->dir = make_path_dir(cwd, data->name);
-		mkdir(data->dir, 0775);
-	}
+		mkdir(pathdir, 0775);
 	return (0);
 }
 
@@ -40,10 +43,66 @@ static char	*make_path_dir(char *cwd, char *name)
 	temp = join(cwd, "/");
 	new_dir = join(temp, name);
 	free(temp);
+	temp = new_dir;
+	new_dir = join(new_dir, "/");
+	free(temp);
 	return (new_dir);
 }
 
 static void	make_structs(cli *data)
 {
+	printf("Building %s\n", data->name);
+	make_file(data->dir, "Makefile");
+	make_file(data->dir, "README.md");
+	make_file(data->dir, ".gitignore");
+	make_dir_structs(data);
+	make_main(data);
+	make_include(data);
+}
 
+static void	make_main(cli *data)
+{
+	char	*pathmain;
+
+	pathmain = join(data->dir, "app/src/");
+	make_file(pathmain, "main.c");
+	free(pathmain);
+}
+
+static void	make_include(cli *data)
+{
+	char	*pathinclude;
+	char	*include;
+
+	include = join(data->name, ".h");
+	pathinclude = join(data->dir, "app/includes/");
+	make_file(pathinclude, include);
+	free(include);
+	free(pathinclude);
+}
+
+static void	make_file(char *path, char *file)
+{
+	char	*pathname;
+
+	pathname = join(path, file);
+	creat(pathname, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	printf("Created file %s\n", pathname);
+	free(pathname);
+}
+
+static void	make_dir_structs(cli *data)
+{
+	char *dirs[3];
+
+	dirs[0] = join(data->dir, "app");
+	dirs[1] = join(data->dir, "app/includes");
+	dirs[2] = join(data->dir, "app/src");
+	for(int i = 0; i < 3; i++)
+	{
+		printf("Created dir %s\n", dirs[i]);
+		make_dir(dirs[i]);
+	}
+	for(int i = 0; i < 3; i++)
+		free(dirs[i]);
 }
